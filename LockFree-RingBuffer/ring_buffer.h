039c9271dc30a1400c62e9e8186cea91cfa0ccff
ChrinovicMu <chrinovicmu@gmail.com>
2025-04-20 23:06:27 +0200

    lock init

---
+++ LockFree-RingBuffer/ring_buffer.h
@@ -0,0 +1,111 @@
+#ifndef RING_BUFFER_H_ 
+#define RING_BUFFER_H_
+
+#include <stdio.h>
+
+
+#define CACHE_LINE_SIZE 64 
+
+static inline unsigned int align32_pow2(unsigned int x)
+{
+    --x; 
+    x |= x >> 1; 
+    x |= x >> 2;
+    x |= x >> 4;
+    x |= x >> 8;
+    x |= x >> 16;
+
+    return x + 1;
+}
+
+typedef struct{
+
+    __attribute__((aligned(CACHE_LINE_SIZE))) unsigned int  cached_head; 
+    __attribute__((aligned(CACHE_LINE_SIZE))) unsigned int  cached_tail;  
+    __attribute__((aligned(CACHE_LINE_SIZE))) unsigned int  head; 
+    __attribute__((aligned(CACHE_LINE_SIZE))) unsigned int  tail;
+    
+    unsigned int capacity; 
+    unsigned int mask;
+    void **buffer;
+}__attribute__((aligned(CACHE_LINE_SIZE))) ring_buffer_t; 
+
+
+/*
+ * NORMAL PUSH OPERATION 
+ 
+int push(ring_buffer_t *const rb, void* value)
+{
+    if((rb>head - rb->tail) == rb->capacity)
+    {
+        return 1; 
+    }
+    rb->buffer[rb->head % rb->capacity] == value; 
+    ++rb->head; 
+
+    return 0; 
+}
+*/ 
+
+/*ATOMIC PUSH OPERATION */ 
+
+int ring_buffer_init(ring_buffer_t *const rb, unsigned int capacity)
+{
+    rb->capacity =align32_pow2(capacity); 
+    rb->mask =  rb->capacity - 1; 
+    rb->head = 0;
+    rb->tail = 0;
+    rb->cached_head = 0;
+    rb->cached_tail = 0; 
+
+    return 1; 
+}
+
+int push(ring_buffer_t *const rb, void *const value)
+{
+
+    __atomic_load(&rb->tail, &rb->cached_tail, __ATOMIC_ACQUIRE);
+
+    if((rb->head - rb->cached_tail) == rb->capacity)
+    {
+        return 0; 
+    }
+
+    rb->buffer[rb->head & rb->mask] = value; 
+    __atomic_fetch_add(&rb->head, 1, __ATOMIC_RELEASE); 
+
+    return 1; 
+}
+
+/*
+ * NORMAL POP OPERATION
+ 
+int pop(ring_buffer_t *const rb, void ** value)
+{
+    if((rb->head - rb->tail) == 0)
+    {
+        return 0; 
+    }
+
+    *value = rb->buffer[rb->tail % rb->capacity]; 
+    ++rb->tail; 
+    return 1; 
+}
+*/ 
+
+int pop(ring_buffer_t *const rb, void ** const value)
+{
+
+    __atomic_load(&rb->head, &rb->cached_head, __ATOMIC_ACQUIRE); 
+
+    if((rb->cached_head - rb->tail) == 0)
+    {
+        return 0; 
+    }
+    *value = rb->buffer[rb->tail & rb->mask];
+    __atomic_fetch_add(&rb->tail, 1, __ATOMIC_RELEASE);
+
+    return 1 ;
+}
+
+#endif
